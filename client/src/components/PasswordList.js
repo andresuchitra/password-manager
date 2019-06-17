f3fb3c1b98af65e057b2563ed51ec5f0e9b2f3b3
Andre Suchitra <89.andre@gmail.com>
2019-06-17 17:16:47 +0700

    Add Loading spinner

--- client/src/components/PasswordList.js
+++ client/src/components/PasswordList.js
@@ -3,6 +3,7 @@ import {Table, Button} from 'react-bootstrap'
 import db from '../api/firestore'
 import Moment from 'moment'
 import {Link} from 'react-router-dom'
+import LoadingSpinner from './LoadingSpinner';
 
 
 function handleDelete(e, id,list, setList) {
@@ -46,39 +47,45 @@ function useFetchPassword() {
 function PasswordList(props) {
     const [list, loading, error, setList] = useFetchPassword();
 
-    return (
-        <Table striped bordered hover variant="dark">
-            <thead>
-                <tr>
-                    <th>No.</th>
-                    <th>URL</th>
-                    <th>Username</th>
-                    <th>Password</th>
-                    <th>Created At</th>
-                    <th>Updated At</th>
-                    <th colSpan={2}>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {list.map((pwd, index) => 
-                    <tr key={index}>
-                        <td>{(index + 1)}</td>
-                        <td>{pwd.url}</td>
-                        <td>{pwd.username}</td>
-                        <td>{pwd.password}</td>
-                        <td>{Moment(pwd.createdat.toString()).format('LLLL')}</td>
-                        <td>{Moment(pwd.updatedat.toString()).format('LLLL')}</td>
-                        <td>
-                            <Link to={'/update/' + pwd.id}><Button variant="info">Update</Button></Link>
-                        </td>
-                        <td>
-                            <Button variant="danger" onClick={ (e) => handleDelete(this, pwd.id, list, setList)  }>Delete</Button>
-                        </td>
+    if(loading) {
+        return <LoadingSpinner />
+    }
+    else {
+
+        return (
+            <Table striped bordered hover variant="dark">
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>URL</th>
+                        <th>Username</th>
+                        <th>Password</th>
+                        <th>Created At</th>
+                        <th>Updated At</th>
+                        <th colSpan={2}>Actions</th>
                     </tr>
-                )}
-            </tbody>
-        </Table>
-    );
+                </thead>
+                <tbody>
+                    {list.map((pwd, index) => 
+                        <tr key={index}>
+                            <td>{(index + 1)}</td>
+                            <td>{pwd.url}</td>
+                            <td>{pwd.username}</td>
+                            <td>{pwd.password}</td>
+                            <td>{Moment(pwd.createdat.toString()).format('LLLL')}</td>
+                            <td>{Moment(pwd.updatedat.toString()).format('LLLL')}</td>
+                            <td>
+                                <Link to={'/update/' + pwd.id}><Button variant="info">Update</Button></Link>
+                            </td>
+                            <td>
+                                <Button variant="danger" onClick={ (e) => handleDelete(this, pwd.id, list, setList)  }>Delete</Button>
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </Table>
+        );
+    }
 }
 
 export default PasswordList
--- client/src/components/UpdatePassword.js
+++ client/src/components/UpdatePassword.js
@@ -3,12 +3,15 @@ import { Container, Row, Col} from 'react-bootstrap'
 import db from '../api/firestore.js'
 import FormPassword from './FormPassword'
 import swal from 'sweetalert2'
+import LoadingSpinner from './LoadingSpinner.js';
 
 function useFetchPassword(id) {
     const [password, setPassword] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
 
     useEffect(() => {
-        console.log('called again...');
+        setLoading(true);
 
         let docRef = db.collection("passwords").doc(id);
 
@@ -25,17 +28,22 @@ function useFetchPassword(id) {
         .catch(function(error) {
             console.log(error);
             swal.fire('Not found', JSON.stringify(error), 'error')
-        });
+        })
+        .finally(function() {
+            setLoading(false);
+        })
     }, [id])
 
     return [
-        password
+        password,
+        loading,
+        error
     ]
 
 }
 
 function UpdatePassword(props) {
-    const [password] = useFetchPassword(props.match.params.id);
+    const [password, loading, error] = useFetchPassword(props.match.params.id);
 
     function handleSubmit(data) {
         let docRef = db.collection("passwords").doc(props.match.params.id)
@@ -57,24 +65,33 @@ function UpdatePassword(props) {
         });
     }
 
-    return (
-        <Container className="py-3">
-            <Row className="justify-content-center">
-                <Col sm={10} lg={5} md={8}>
-                    <Row className="mb-4 text-center">
-                        <Col>
-                            <h1>Update Password</h1>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <FormPassword password={password} onSubmit={handleSubmit}></FormPassword>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-        </Container>
-    );
+    if(loading) {
+        return (
+            <div className="mt-5 w-100">
+                <LoadingSpinner />
+            </div>
+        )
+    }
+    else {
+        return (
+            <Container className="py-3">
+                <Row className="justify-content-center">
+                    <Col sm={10} lg={5} md={8}>
+                        <Row className="mb-4 text-center">
+                            <Col>
+                                <h1>Update Password</h1>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <FormPassword password={password} onSubmit={handleSubmit}></FormPassword>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
 
 export default UpdatePassword
